694212cb10e05dc3ad32e8076d81a8465dd357a3
Joqueiroz <joaoqueiroz964@gmail.com>
2024-08-28 14:11:54 -0300

    Atividade 28-08

--- Aula-21-08/src/index.js
+++ Aula-21-08/src/index.js
@@ -1,27 +1,62 @@
-const express = require("express"); //Importa o módulo Express
+const express = require("express");
 
-//Define uma classe para organizar a lógica da aplicação
+//Define uma classe para organizar a logica da aplicação
 class AppController {
   constructor() {
-    // Cria uma nova instância do Express dentro da classe
+    //Cria uma nova instancia do Express dentro da classe
     this.express = express();
-    //Chama o método middlewares para configurar os middlewares
+    //Chama o metodo middlewares para configurar os middlewares
     this.middlewares();
-    //Chama o método routes para definir as rotas da API
+    //Chama o metodo routes para definir as rotas da Api
     this.routes();
   }
   middlewares() {
     //Permitir que a aplicação receba dados em formato JSON nas requisições
     this.express.use(express.json());
   }
+
   //Define as rotas da nossa API
   routes() {
-    // Define uma rota GET para o caminho health
+    const users = [];
+
+    this.express.post("/users", (req, res) => {
+      const { id, nome, email, senha } = req.body;
+      users.push({ id, nome, email, senha });
+      res.status(200).send({ message: "Usuario cadastrado com sucesso" });
+    });
+
+    this.express.post("/auth", (req, res) => {
+      const {email, senha } = req.body;
+      const login = users.find((login) => login.email == email && login.senha == senha);
+      if(login) {
+        res.status(200).send({ message: "Autenticação deu certa" });
+      }
+      else{
+        res.status(404).send({ message: "Usuario não cadastrado" });
+      }
+
+
+    });
+
+
+
+    this.express.get("/users/:id", (req, res) => {
+      const { id } = req.params;
+      const user = users.find((user) => user.id == id);
+      if(user){
+        res.status(200).send(user)
+      }
+      else{
+        res.status(400).send({message: "Usuario não encontrado"})
+      }
+    });
+
+    //Define uma rota GET para o caminho health
     this.express.get("/health/", (req, res) => {
-      res.send({ status: "OK", nome: "Joao" });
-    });//Essa rota é usada para verificar se a API está OK
+      res.send({ status: "OK" });
+    }); //Essa rota é usada para verificar se a Api esta ok
   }
 }
 
-//Exportando a instância de Express configurada, para que seja acessada em outros arquivos
+//Exportando a instancia de Express configurada, para que seja acessada em outros arquivos
 module.exports = new AppController().express;
